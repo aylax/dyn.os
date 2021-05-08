8db2328d22c576bcce44c40d71c4aa4e048f7979
AyLax <zhoubye@foxmail.com>
2021-05-08 23:42:30 +0800

    c udp conn

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ music player
 cargo run --example main # for examples/main.rs
 cargo test # for test
 ```
+
+
+```sh
+# test upd 
+cargo run lynn # udp socket listen on [::1]:6000
+# echo "send sth..." > /dev/udp/localhost/6000
+exec 8<>/dev/udp/localhost/6000 # link dev/udp to file descriptor :8
+echo "send sth...">&8 # write to fd :8
+cat <&8 # out :8 content
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::io;
+use async_std::task;
+use async_std::net::UdpSocket;
+
+fn main() -> io::Result<()> {
+    task::block_on(async_run())
 }
+
+
+async fn async_run() -> io::Result<()> {
+    let socket = UdpSocket::bind("[::1]:6000").await?;
+    println!("Listening on {}", socket.local_addr()?);
+    let mut buf = vec![0; 1024];
+    let mut cnt = 0;
+    loop {
+        let (n, peer) = socket.recv_from(&mut buf).await?;
+        cnt = cnt + 1;
+        println!("{}: Recv {} bytes from {}", cnt, n, peer);
+        let message : &[u8] = b"Received!!";
+        socket.send_to(message, peer).await?;
+        if cnt > 5 { break; }
+    } 
+    Ok(())
+}
+
+
