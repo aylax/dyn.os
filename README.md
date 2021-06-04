6e563f73d8f76d08c1626ad2cd533871dcf48f2e
aylax <zhoubye@foxmail.com>
2021-06-04 23:15:25 +0800

    upate lynn

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # lynn
 
 ```bash
+rustup target list | grep riscv64
+rustup target add riscv64gc-unknown-none-elf
 cargo install cargo-binutils
 rustup component add llvm-tools-preview
 qemu-riscv64 target/riscv64gc-unknown-none-elf/debug/lynn; echo $?
--- src/main.rs
+++ src/main.rs
@@ -4,65 +4,28 @@
 
 mod lang_items;
 
-fn syscall(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+
+const SYSCALL_EXIT: usize = 93;
+fn syscall(id: usize, arg0: usize, arg1: usize, arg2: usize) -> isize {
     let mut ret: isize;
     unsafe {
         llvm_asm!("ecall"
             : "={x10}" (ret)
-            : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+            : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (id)
             : "memory"
             : "volatile"
+
         );
     }
     ret
 }
 
 
-const SYS_EXIT: usize = 93;
-pub fn sys_exit(xstate: usize) -> usize {
-    syscall(SYS_EXIT, xstate, 0 , 0)
-} 
-
-const SBI_SHUTDOWN: usize = 8;
-pub fn shutdown() -> ! {
-    syscall(SBI_SHUTDOWN, 0, 0, 0);
-    panic!("It should shutdown!");
-}
-
-
-const SYSCALL_WRITE: usize = 64;
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-  syscall(SYSCALL_WRITE, fd, buffer.as_ptr() as usize, buffer.len())
-}
-
-
-struct Stdout;
-impl core::fmt::Write for Stdout {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
-    }
-}
-pub fn print(args: core::fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
+pub fn sys_exit(xstate: isize) -> isize {
+    syscall(SYSCALL_EXIT, xstate as usize, 0, 0)
 }
 
 #[no_mangle]
 extern "C" fn _start() {
-    shutdown()
+    sys_exit(9);
 }
