a33c0bd8e2e8491e8e71c0a9cf8126d3a00f26c8
aylax <zhoubye@foxmail.com>
2021-06-04 17:13:10 +0800

    update lynn

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lynn
 
+```bash
+cargo install cargo-binutils
+rustup component add llvm-tools-preview
+qemu-riscv64 target/riscv64gc-unknown-none-elf/debug/lynn; echo $?
+```
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,31 @@
-use bevy::prelude::*;
+#![no_std]
+#![no_main]
+#![feature(llvm_asm)]
 
-struct Person;
+mod lang_items;
 
-struct Name(String);
 
-fn hello_world() {
-    println!("hello world!");
-}
 
-pub struct HelloPlugin;
+const SYS_EXIT: usize = 93;
 
-impl Plugin for HelloPlugin {
-    fn build(&self, _app: &mut AppBuilder) {
-        // add things to your app here
-        println!("hello plugin");
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        llvm_asm!("ecall"
+            : "={x10}" (ret)
+            : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
+            : "memory"
+            : "volatile"
+        );
     }
+    ret
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
-    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYS_EXIT, [xstate as usize, 0 , 0])
 }
 
-struct GreetTimer(Timer);
-
-fn greet_people(
-    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
+#[no_mangle]
+extern "C" fn _start() {
+    sys_exit(9);
 }
-
-
-fn main() {
-    App::build()
-        .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-        .add_startup_system(add_people.system())
-        .add_system(hello_world.system())
-        .add_system(greet_people.system())
-        .run();
-}
-
